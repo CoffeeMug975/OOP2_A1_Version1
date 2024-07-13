2d6832bee65a1acac44e84bc286d6d84473c77e2
Daniel A <156850549+CoffeeMug975@users.noreply.github.com>
2024-07-13 00:03:03 -0600

    init

---
+++ ModernAppliances/A1ModernAppliances/Dishwasher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForAssignment1
+{
+    public class Dishwasher : Appliance
+    {
+        string Feature {  get; set; }
+        string SoundRating { get; set; }
+
+        public Dishwasher (int itemNumber, string brand, int quantity, int wattage, string color, double price, string feature, string soundRating)
+            :base(itemNumber, brand, quantity, wattage, color, price)
+        {
+            ItemNumber = itemNumber;
+            Brand = brand;
+            Quantity = quantity;
+            Wattage = wattage;
+            Color = color;
+            Price = price;
+            Feature = feature;
+            SoundRating = soundRating;
+        }
+
+        public override string ToString()
+        {
+            string obj = ($"ItemNumber: {ItemNumber}\nBrand: {Brand}\nQuantity: {Quantity}\nWattage: {Wattage}\nColor: {Color}\nPrice: {Price}" +
+                $"\nFeature: {Feature}\nSoundRating: {SoundRating}");
+            return obj;
+        }
+    }
+}
---
+++ ModernAppliances/A1ModernAppliances/Refrigerator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForAssignment1
+{
+    public class Refrigerator : Appliance
+    {
+        int NumberOfDoors { get; set; }
+        int Height { get; set; }
+        int Width { get; set; }
+
+        public Refrigerator(int itemNumber, string brand, int quantity, int wattage, string color, double price, int numberOfDoors,
+            int height, int width) : base(itemNumber, brand, quantity, wattage, color, price)
+        {
+            ItemNumber = itemNumber;
+            Brand = brand;
+            Quantity = quantity;
+            Wattage = wattage;
+            Color = color;
+            Price = price;
+            NumberOfDoors = numberOfDoors;
+            Height = height;
+            Width = width;
+
+        }
+
+        public override string ToString()
+        {
+            string obj = ($"ItemNumber: {ItemNumber}\nBrand: {Brand}\nQuantity: {Quantity}\nWattage: {Wattage}\nColor: {Color}\nPrice: {Price}" +
+                $"\nNumber of Doors: {NumberOfDoors}\nHeight: {Height}\nWidth: {Width}");
+            return obj;
+        }
+    }
+}
